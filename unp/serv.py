f457361fa567b6a00ebffd7873b90012e6f77a04
vhqr0 <zq_cmd@163.com>
2022-05-02 09:58:47 +0800

    serv.py update

--- unp/serv.py
+++ unp/serv.py
@@ -2,9 +2,10 @@
 
 import argparse
 import ipaddress
+import signal
 import socket
-import time
 import sys
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', action='store_const', dest='mode', const='client')
@@ -26,58 +27,71 @@ parser.add_argument('-T',
                     dest='socktype',
                     const=socket.SOCK_STREAM)
 parser.add_argument('-p', '--port', default='13')
+parser.add_argument('-t', '--timeout', type=int, default=1)
 parser.add_argument('address')
 args = parser.parse_args()
 
+mode = args.mode or 'server'
 family = args.family or socket.AF_INET
 socktype = args.socktype or socket.SOCK_DGRAM
-endpoint = socket.getaddrinfo(args.address,
-                              args.port,
-                              family=family,
-                              type=socktype)[0][-1]
-mode = args.mode or 'server'
+ep = socket.getaddrinfo(args.address, args.port, family=family,
+                        type=socktype)[0][-1]
+timeout = args.timeout
 
-sockfd = socket.socket(family, socktype)
 
-if mode == 'client':
+def client():
+    sockfd = socket.socket(family, socktype)
     if socktype == socket.SOCK_STREAM:
-        sockfd.connect(endpoint)
+        sockfd.connect(ep)
         sys.stdout.buffer.write(sockfd.recv(4096))
     elif socktype == socket.SOCK_DGRAM:
-        if family == socket.AF_INET and endpoint[0] == '255.255.255.255':
+        if family == socket.AF_INET and ep[0] == '255.255.255.255':
             sockfd.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sockfd.sendto(b'\x00', endpoint)
+        sockfd.sendto(b'\x00', ep)
+        if timeout > 0:
+            signal.signal(signal.SIGALRM, lambda _no, _f: sys.exit(0))
+            signal.alarm(timeout)
         while True:
-            buf, servendpoint = sockfd.recvfrom(4096)
+            buf, servep = sockfd.recvfrom(4096)
             sys.stdout.buffer.write(
-                f'recvfrom {servendpoint[0]}, port {servendpoint[1]}: '.encode(
-                ))
+                f'recvfrom {servep[0]}, port {servep[1]}: '.encode())
             sys.stdout.buffer.write(buf)
             sys.stdout.flush()
     sockfd.close()
-else:
+
+
+def server():
+    servep = ep
+    sockfd = socket.socket(family, socktype)
     if family == socket.AF_INET:
-        if ipaddress.IPv4Address(endpoint[0]).is_multicast:
+        if ipaddress.IPv4Address(ep[0]).is_multicast:
             sockfd.setsockopt(
                 socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
-                socket.inet_pton(socket.AF_INET, endpoint[0]) + b'\x00' * 4)
-            endpoint = ('', endpoint[1])
+                socket.inet_pton(socket.AF_INET, ep[0]) + b'\x00' * 4)
+            servep = ('', ep[1])
     elif family == socket.AF_INET6:
-        if ipaddress.IPv6Address(endpoint[0]).is_multicast:
+        if ipaddress.IPv6Address(ep[0]).is_multicast:
             sockfd.setsockopt(
                 socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP,
-                socket.inet_pton(socket.AF_INET6, endpoint[0]) + b'\x00' * 16)
-            endpoint = ('', endpoint[1])
-    sockfd.bind(endpoint)
+                socket.inet_pton(socket.AF_INET6, ep[0]) + b'\x00' * 16)
+            servep = ('', ep[1])
+    sockfd.bind(servep)
     if socktype == socket.SOCK_STREAM:
         sockfd.listen()
+    signal.signal(signal.SIGINT, lambda _no, _f: sys.exit(0))
     while True:
         if socktype == socket.SOCK_STREAM:
-            clifd, cliendpoint = sockfd.accept()
-            print(f'accept {cliendpoint[0]}, port {cliendpoint[1]}')
+            clifd, cliep = sockfd.accept()
+            print(f'accept {cliep[0]}, port {cliep[1]}')
             clifd.send(f'{time.ctime()}\r\n'.encode())
             clifd.close()
         elif socktype == socket.SOCK_DGRAM:
-            _, cliendpoint = sockfd.recvfrom(4096)
-            print(f'recvfrom {cliendpoint[0]}, port {cliendpoint[1]}')
-            sockfd.sendto(f'{time.ctime()}\n'.encode(), cliendpoint)
+            _, cliep = sockfd.recvfrom(4096)
+            print(f'recvfrom {cliep[0]}, port {cliep[1]}')
+            sockfd.sendto(f'{time.ctime()}\n'.encode(), cliep)
+
+
+if mode == 'client':
+    client()
+elif mode == 'server':
+    server()
